53c7bde8c7e790ddc76361386131777828e5b0e8
SylwiaJuda <93827662+SylwiaJuda@users.noreply.github.com>
2022-10-29 18:25:24 +0200

    Create Spreads.java

---
+++ spring_101/Spreads.java
@@ -0,0 +1,13 @@
+package pl.pjatk.syljud;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Spreads {
+    public Spreads(){
+        System.out.println("Hello from Spreads");
+    }
+    public void Stickers(){
+        System.out.println("Stickers");
+    }
+}
