c7a3e5d00caf609f7dca0fc9f84d0440614897d7
SylwiaJuda <93827662+SylwiaJuda@users.noreply.github.com>
2022-10-29 18:24:39 +0200

    Create SyljudApplication.java

---
+++ spring_101/SyljudApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.syljud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyljudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyljudApplication.class, args);
+	}
+
+}
